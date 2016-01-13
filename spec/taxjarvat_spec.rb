b58418a19d18fc2d8bcb9d5f3e6af8f9b2617cda
Alec Carper <xarious@gmail.com>
2016-01-13 15:30:52 -0700

    service availble specs

--- spec/taxjarvat_spec.rb
+++ spec/taxjarvat_spec.rb
@@ -27,4 +27,30 @@ describe TaxJarVat do
       expect(response[:format_valid]).to eq(true)
     end
   end
+
+  context 'service is available' do
+    it 'returns the expected information with a valid VAT', vcr: { cassette_name: 'requests/service_available_valid_vat', record: :none } do
+      response = TaxJarVat.validate('GB333289454')
+      expect(response[:format_valid]).to eq(true)
+
+      validation = response[:validation]
+      expect(validation[:country_code]).to eq('GB')
+      expect(validation[:vat_number]).to eq('333289454')
+      expect(validation[:valid]).to eq(true)
+      expect(validation[:name]).to eq('BRITISH BROADCASTING CORPORATION')
+      expect(validation[:address].gsub("\n", " ")).to eq('FAO ALEX FITZPATRICK BBC GROUP VAT MANAGER THE LIGHT HOUSE (1ST FLOOR) MEDIA VILLAGE, 201 WOOD LANE LONDON W12 7TQ')
+    end
+
+    it 'returns the expected response for a valid but unknown VAT', vcr: { cassette_name: 'requests/service_available_valid_but_unknown_vat', record: :none } do
+      response = TaxJarVat.validate('GB999999999')
+      expect(response[:format_valid]).to eq(true)
+
+      validation = response[:validation]
+      expect(validation[:country_code]).to eq('GB')
+      expect(validation[:vat_number]).to eq('999999999')
+      expect(validation[:valid]).to eq(false)
+      expect(validation[:name]).to eq('---')
+      expect(validation[:address]).to eq('---')
+    end
+  end
 end
